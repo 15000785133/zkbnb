1ce64edb4a2e51e7e5b1627b29993baa32dc4dcb
markc <mark.c@binance.com>
2023-03-08 23:16:27 +0300

    rand  panic

--- common/feature.go
+++ common/feature.go
@@ -26,11 +26,13 @@ func Test(feature string, functionNameConfig string, functionName string) {
 	if functionNameConfig != functionName {
 		return
 	}
-	if feature == "sleep" {
-		logx.Infof("%s->%s", functionNameConfig, feature)
-		time.Sleep(2 * time.Minute)
-	} else if feature == "panic" {
-		logx.Severef("%s->%s", functionNameConfig, feature)
-		panic(functionNameConfig + "->" + feature)
+	if time.Now().UnixMilli()%2 == 0 {
+		if feature == "sleep" {
+			logx.Infof("%s->%s", functionNameConfig, feature)
+			time.Sleep(2 * time.Minute)
+		} else if feature == "panic" {
+			logx.Severef("%s->%s", functionNameConfig, feature)
+			panic(functionNameConfig + "->" + feature)
+		}
 	}
 }
